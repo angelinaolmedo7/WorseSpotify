fbdd6e585b2cda202110a3fdfc2eb380d619c59a
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-09-21 02:50:44 -0700

    Spotify auth, get user
    yeah i know the api key is naked ill fix that later

--- WorseSpotify/AuthViewController.swift
+++ WorseSpotify/AuthViewController.swift
@@ -37,7 +37,74 @@ class AuthViewController: UIViewController, ASWebAuthenticationPresentationConte
     }
     
     func startSession(code: String) {
+        let url = URL(string: "https://accounts.spotify.com/api/token")!
+        var request = URLRequest(url: url)
+        request.setValue("application/x-www-from-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        var urlComponents = URLComponents()
+        urlComponents.queryItems = [
+            URLQueryItem(name: "grant_type", value: "authorization_code"),
+            URLQueryItem(name: "code", value: code),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI.absoluteString),
+            URLQueryItem(name: "client_id", value: Constants.clientID),
+            URLQueryItem(name: "client_secret", value: Constants.client_secret)
+        ]
+        request.httpBody = urlComponents.query?.data(using: .utf8)
         
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, let response = response as? HTTPURLResponse, error == nil else {
+                print("ERROR", error ?? "uhhh i dunno man")
+                return
+            }
+            
+            guard (200...299) ~= response.statusCode else { // not sure what ~= means?
+                print("status code: \(response.statusCode)")
+                print("response = \(response)")
+                return
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+            do {
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary
+                print(jsonResult!)
+                let token = jsonResult?["access_token"] as! String
+                print(token)
+                self.getUser(accessToken: token)
+            }
+            catch let error as NSError {
+                print(error.debugDescription)
+            }
+        }
+        task.resume()
+    }
+    
+    func getUser(accessToken: String) {
+        let url = URL(string: "https://api.spotify.com/v1/me")!
+        var request = URLRequest(url: url)
+        request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, let response = response as? HTTPURLResponse, error == nil else {
+                print("error", error ?? "i cant help u with this one buddy")
+                return
+            }
+            
+            guard (200...299) ~= response.statusCode else { // not sure what ~= means?
+                print("status code: \(response.statusCode)")
+                print("response = \(response)")
+                return
+            }
+            
+            do {
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary
+                print(jsonResult!)
+            }
+            catch let error as NSError {
+                print(error.debugDescription)
+            }
+        }
+        task.resume()
     }
 
     /*
